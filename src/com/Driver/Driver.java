86e23cda13f9eaa9d34e73aedb3ad1613594889c
CBlumini <72533588+CBlumini@users.noreply.github.com>
2022-01-05 20:55:12 -0800

    Addressing bugs

---
+++ src/com/Driver/Driver.java
@@ -0,0 +1,23 @@
+package com.Driver;
+
+import com.LinkedList.LinkedList;
+
+public class Driver {
+    public static void main(String[] args){
+        LinkedList <Integer> testList = new LinkedList<>();
+
+        testList.addAtIndex(1, 0);
+        testList.addAtIndex(2, 0);
+        testList.addAtIndex(3, 0);
+        testList.addAtIndex(4, 0);
+        testList.addAtIndex(5, 0);
+        testList.addAtIndex(5, 5);
+        //testList.removeAtIndex(0);
+
+
+
+
+
+
+    }
+}
--- src/com/LinkedList/LinkedList.java
+++ src/com/LinkedList/LinkedList.java
@@ -27,15 +27,14 @@ public class LinkedList<T> implements List<T> {
         if (size == 0) {
             head = new Node<T>(data);
             tail = head;
-            size++;
         }
 
-        //add to the front once the first node is created
-        if (index == 0) {
+        if (index == 0 && size > 0) {
+            //add to the front once the first node is created
             head = new Node <T> (data, head);
-            size++;
 
-        } else { //add to other locations
+        } else if (size > 0 && index != size) {
+            //add to other locations
             //create a temporary node that equals the head
             Node<T> temp = head;
             //cycle through the list to get to the location
@@ -49,8 +48,18 @@ public class LinkedList<T> implements List<T> {
             newNode.setNext(temp.getNext());
             //set the pointer (the upstream part of the list to look at the new node
             temp.setNext(newNode);
-            size++;
+            //if adding at the end (tail)
+        } else {
+            //create a new node for the data
+            Node<T> newNode = new Node <T> (data);
+            //point the existing tail to the new node
+            tail.setNext(newNode);
+            //set the new node as the tail
+            tail = newNode;
         }
+
+        //increment the size
+        size++;
 
     }
 
@@ -80,19 +89,32 @@ public class LinkedList<T> implements List<T> {
             throw new IllegalArgumentException("Your index is out of bounds");
         }
 
+        //create a temp node
         Node<T> temp = head;
+        //if the requested node is the head
         if (index == 0) {
             T removedData = head.getData();
             head = head.getNext();
+            size--;
+            //set the tail to null if the list reaches size 0 after a remove operation
+            if (size == 0) {
+                tail = null;
+            }
             return removedData;
         }
+
+        //if the requested node isn't the head
+        //cycle through the nodes
         for (int i = 0; i < index-1; i++) {
             temp = temp.getNext();
         }
+        //creat another temp node
         Node<T> tempTemp = temp;
+        //check for null data
         if (temp == null || temp.getNext() == null) {
             return null;
         }
+
         Node<T> next = temp.getNext().getNext();
         temp.setNext(next);
         size--;
